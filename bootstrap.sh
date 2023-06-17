455d10be798545b3ef285afafe41f21630c1cafb
Alexander Lebedev <lebedev@0x4e3.ru>
2023-06-17 20:25:29 +0300

    Add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Exporting the specific shell we want to work with:
+
+SHELL='/bin/bash'
+export SHELL
+
+# Standard dotbot pre-configuration:
+
+readonly DOTBOT_DIR='dotbot'
+readonly DOTBOT_BIN='bin/dotbot'
+readonly BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+readonly ARGS="$@"
+
+cd "$BASEDIR"
+git submodule sync --quiet --recursive
+git submodule update --init --recursive
+
+# Linking dotfiles:
+
+run_dotbot () {
+  local config
+  config="$1"
+
+  "$BASEDIR/$DOTBOT_DIR/$DOTBOT_BIN" \
+    -d "$BASEDIR" \
+    --plugin-dir dotbot-brewfile \
+    --plugin-dir dotbot-git \
+    --plugin-dir dotbot-pip \
+    -c "$config" $ARGS
+}
+
+run_dotbot 'installation/brew.yaml' || true
+run_dotbot 'installation/configs.yaml' || true
+run_dotbot 'installation/python.yaml' || true
+run_dotbot 'installation/iTerm.yaml' || true
